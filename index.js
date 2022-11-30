fbc1d019f5427c79a5fe385e6388d521a9f7220c
Danah <osta.danah@gmail.com>
2022-12-01 01:36:33 +0200

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+
+class Polygon{
+    constructor(array){
+        this.array = array;
+    }
+    
+    get countSides(){
+        return this.array.length
+    }
+
+    get perimeter(){
+        return this.array.reduce( ( total , currentValue ) => total + currentValue , 0)
+    }
+}
+
+class Triangle extends Polygon{
+    get isValid(){
+        let side1 = this.array[0];
+        let side2 = this.array[1];
+        let side3 = this.array[2];
+
+       if ( side1 + side2 > side3 && side2 + side3 > side1 && side1 + side3 > side2 ) {
+        return true;
+       } else {
+        return false;
+       }        
+    }
+}
+
+class Square extends Polygon {
+    get area(){
+        let side = this.array[0];
+        return Math.pow( side , 2 )
+    }
+
+    get isValid(){
+        return this.array.every( side => side === this.array[0] )
+    }
+}
